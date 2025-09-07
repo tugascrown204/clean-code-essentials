e8f347e792f378dd0ff39336cd45fbaa7a6c7490
tugascrown204 <gajdertevet@hotmail.com>
2025-09-07 22:58:09 +0330

    Added a comment to the main function to clarify its purpose.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log('Welcome to Clean Code Essentials');
 
 function main() {
     // Application logic goes here
+    // This function serves as the main entry point for the app's logic.
     console.log('Application logic implemented.');
 }
 
